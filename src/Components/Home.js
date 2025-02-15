cba620a705eb543a2f5691502a910ade6cebd36f
SONALI2052 <120799669+SONALI2052@users.noreply.github.com>
2025-02-15 22:06:05 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function Home() {
-  return <h2 style={{ color: "blue" }}>Welcome to the Homepage</h2>;
+  return (
+    <div>
+      <h2 style={{ color: "blue" }}>Welcome to the Homepage</h2>
+      <p style={{ maxWidth: "600px", textAlign: "justify",color:"yellow" }}>
+         eTechnolab Pvt Ltd, Pune, is a leading software development company
+         specializing in web, mobile, and AI-driven solutions. 
+         With a focus on innovation and scalability, eTechnolab helps businesses
+         grow through cutting-edge technology and customized digital solutions.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
-
-
